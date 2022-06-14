1e130092972745a6f8cd46b1cdf9705414090442
21Kgold <claudiarpa@gmail.com>
2022-06-14 11:08:27 -0700

    update

--- sort.h
+++ sort.h
@@ -19,11 +19,23 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Auxiliary functions*/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+size_t knuth_sequence(size_t maximum);
+
+/* Sorting functions*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
